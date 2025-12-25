43f60811cb21e9d8039b3dd9ef9910b1bf2458ae
sulaimonazeez <olaniyisulaimon221@gmail.com>
2025-12-25 08:01:49 -0600

    committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/admin/login.jsx";
 import AdminDashboard from "./components/admin/AdminDashboard.jsx";
